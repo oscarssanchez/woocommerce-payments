c81773a1f02f56d595623db217fabe08188d4def
Achyuth Ajoy <achyuthajoy@gmail.com>
2021-11-11 17:43:19 +0530

    Remove server start up script from up.sh

--- tests/e2e/env/up.sh
+++ tests/e2e/env/up.sh
@@ -5,6 +5,3 @@ set -e
 
 step "Starting client containers"
 docker-compose -f "$E2E_ROOT/env/docker-compose.yml" start
-
-step "Starting server containers"
-docker-compose -f "$E2E_ROOT/deps/wcp-server/docker-compose.yml" start
